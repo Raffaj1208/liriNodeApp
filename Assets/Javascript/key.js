b09b88d8c42b2a5c7a068c795a293526427a8161
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-06-05 20:32:23 -0700

    axios and spotify

--- Assets/Javascript/key.js
+++ Assets/Javascript/key.js
@@ -5,3 +5,15 @@ exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
+//..
+let spotify = require('node-spotify-api');
+let spotify = new spotify({
+    id: SPOTIFY_ID,
+    secret: SPOTIFY_SECRET
+});
+spotify.search({type:'track',query:'All the Small Things'}, function(error, data){
+    if (error){
+        return console.log('Error Ocurred' + error);
+    }
+    console.log(data);
+});
