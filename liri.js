b8c23e12250bfd5a6dc5d3489457cdd0bcec1173
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-06-09 13:43:18 -0700

    NOde Spotify Api

--- liri.js
+++ liri.js
@@ -1,3 +1,11 @@
 let dotEnv = require("dotenv").config();
 let keys = require("./keys");
+let Spotify = require('node-spotify-api');
 let spotify = new Spotify(keys.spotify);
+//..
+spotify.search({type: 'track', query: 'All the Small Things'}, function(error, data){
+    if (error){
+        return console.log('Error occurred:' + error);
+    }
+    console.log(data);
+});
