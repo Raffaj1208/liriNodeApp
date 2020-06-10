402483ab40ad475de399c577630407471dd1b0a0
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-06-10 09:36:10 -0700

    ..

--- liri.js
+++ liri.js
@@ -2,11 +2,38 @@ let dotEnv = require("dotenv").config();
 let keys = require("./keys");
 let Spotify = require('node-spotify-api');
 let spotify = new Spotify(keys.spotify);
-const axios = require("axios");
+let command = process.argv[2];
+let search = process.argv[3];
 //..
-spotify.search({type: 'track', query: 'All the Small Things'}, function(error, data){
-    if (error){
-        return console.log('Error occurred:' + error);
+function action(){
+    switch (command) {
+        case "spotify-this-song":
+        spotifyThis(search);
+        break;
     }
-    console.log(data);
-});
+};
+function spotifyThis(search){
+    let searchTrack;
+    if (command === undefined){
+        searchTrack = "The Sign Ace of Base";
+    } else {
+        searchTrack = search;
+    }
+    spotify.search({
+        type: 'track',
+        query: 'All the Small Things'},
+        function(error, data){
+        if (error){
+            return console.log('Error occurred:' + error);
+        } else {
+            console.log("\n---------------------------------------------------\n");
+            console.log("Artist:" + data.tracks.items[0].artists[0].name);
+            console.log("Song:" + data.tracks.items[0].name);
+            console.log("Preview:" + data.tracks.items[3].preview_url);
+            console.log("Album:" + data.tracks.items[0].album.name);
+            console.log("\n---------------------------------------------------\n");
+        }
+        console.log(data);
+    });
+};
+action();
